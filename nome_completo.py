eb5afcb4df1d67d440de073d541f50c912e4ec6d
M-vitor <catce.2023111mtds0014@aluno.ifpi.edu.br>
2023-03-10 22:48:39 -0300

    adicionei funcao main

--- nome_completo.py
+++ nome_completo.py
@@ -1,4 +1,8 @@
-nome = input("Escreva seu primeiro nome:").strip()
-sobrenome = input("Escreva seu sobrenome :").strip()
-n_c = 'Ola, '+ nome + ' ' + sobrenome + '!'
-print(n_c)
+def main():
+    nome = input("Escreva seu primeiro nome:").strip()
+    sobrenome = input("Escreva seu sobrenome :").strip()
+    n_c = 'Ola, '+ nome + ' ' + sobrenome + '!'
+    print(n_c)
+
+if __name__ == '__main__':
+    main()
--- soma_multiplicacao.py
+++ soma_multiplicacao.py
@@ -1,5 +1,9 @@
-num1 = float(input())
-num2 = float(input())
+def main():
+    num1 = float(input('Digite o primeiro valor: '))
+    num2 = float(input('Digite o segundo valor: '))
 
-print("A soma desses valores é:",num1 + num2)
-print("A multiplicaçao desses valores é:", num1 * num2)
+    print("A soma desses valores é:",num1 + num2)
+    print("A multiplicaçao desses valores é:", num1 * num2)
+
+if __name__ == '__main__':
+    main()
